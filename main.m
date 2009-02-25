eb663ea273c0f32096e754d942a8b6e5b91b8b07
Prakash Raman <prakash.raman.ka@gmail.com>
2009-02-25 19:04:31 +0530

    Empty project

---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  pux
+//
+//  Created by Prakash Raman on 25/02/09.
+//  Copyright __MyCompanyName__ 2009. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
